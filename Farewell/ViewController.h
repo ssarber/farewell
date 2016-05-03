b337d160e75e64cd42a15d20e01971bf2a79480a
Stan Sarber <ssarber@gmail.com>
2016-05-03 12:05:23 -0700

    Initial Commit

---
+++ Farewell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Farewell
+//
+//  Created by Zook Gek on 5/3/16.
+//  Copyright © 2016 Stan Sarber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FarewellTests/FarewellTests.m
@@ -0,0 +1,39 @@
+//
+//  FarewellTests.m
+//  FarewellTests
+//
+//  Created by Zook Gek on 5/3/16.
+//  Copyright © 2016 Stan Sarber. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FarewellTests : XCTestCase
+
+@end
+
+@implementation FarewellTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
